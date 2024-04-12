554134475f3d7f5835039cb4e845100980939d4d
Ali Qadir <94987050+ALI-QADIR@users.noreply.github.com>
2024-04-12 22:43:15 +0530

    Create Necessary Structure
    Create Necessary Structure for server authoritative movement.

---
+++ Assets/_Scripts/Utils/CircularBuffer.cs
@@ -0,0 +1,20 @@
+namespace Assets._Scripts.Utils
+{
+    public class CircularBuffer<T>
+    {
+        private T[] _buffer;
+        private int _bufferSize;
+
+        public CircularBuffer(int bufferSize)
+        {
+            _bufferSize = bufferSize;
+            _buffer = new T[_bufferSize];
+        }
+
+        public void Add(T item, int index) => _buffer[index % _bufferSize] = item;
+
+        public T Get(int index) => _buffer[index % _bufferSize];
+
+        public void Clear() => _buffer = new T[_bufferSize];
+    }
+}
